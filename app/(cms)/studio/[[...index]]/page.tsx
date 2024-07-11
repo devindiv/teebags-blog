4b08d62057e0264e36cde1a02ff93ffc8671009c
devindiv <57625678+devindiv@users.noreply.github.com>
2024-07-11 15:43:09 +0530

    route fixed

--- app/(cms)/studio/[[...index]]/page.tsx
+++ app/(cms)/studio/[[...index]]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import sanityConfig from "@/sanity.config";
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -10,8 +11,7 @@
  */
 
 import { NextStudio } from "next-sanity/studio";
-import config from "../../../sanity.config";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <NextStudio config={sanityConfig} />;
 }
